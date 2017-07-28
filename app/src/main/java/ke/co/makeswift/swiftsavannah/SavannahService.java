5f33931fd0d63c11a14fedcca4dc6e486abc57ef
robertny <robertny@ondemand.co.ke>
2017-07-28 09:01:15 +0300

    livewallpaper active, dummy settings

--- app/src/main/java/ke/co/makeswift/swiftsavannah/SavannahService.java
+++ app/src/main/java/ke/co/makeswift/swiftsavannah/SavannahService.java
@@ -1,7 +1,10 @@
 package ke.co.makeswift.swiftsavannah;
 
+import android.graphics.Canvas;
 import android.graphics.Movie;
+import android.os.Handler;
 import android.service.wallpaper.WallpaperService;
+import android.view.SurfaceHolder;
 
 import com.orhanobut.logger.Logger;
 
@@ -16,7 +19,7 @@ public class SavannahService extends WallpaperService {
     public WallpaperService.Engine onCreateEngine() {
         try {
             Movie movie = Movie.decodeStream(
-                    getResources().getAssets().open("girl.gif"));
+                    getResources().getAssets().open("savannah.gif"));
 
             return new GIFWallpaperEngine(movie);
         }catch(Exception e){
@@ -24,4 +27,61 @@ public class SavannahService extends WallpaperService {
             return null;
         }
     }
+
+    public class GIFWallpaperEngine extends WallpaperService.Engine {
+        private final int frameDuration = 20;
+
+        private SurfaceHolder holder;
+        private Movie movie;
+        private boolean visible;
+        private Handler handler;
+
+        public GIFWallpaperEngine(Movie movie) {
+            this.movie = movie;
+            handler = new Handler();
+        }
+
+        @Override
+        public void onCreate(SurfaceHolder surfaceHolder) {
+            super.onCreate(surfaceHolder);
+            this.holder = surfaceHolder;
+        }
+        private Runnable drawGIF = new Runnable() {
+            public void run() {
+                draw();
+            }
+        };
+
+        private void draw() {
+            if (visible) {
+                Canvas canvas = holder.lockCanvas();
+                canvas.save();
+                // Adjust size and position so that
+                // the image looks good on your screen
+                canvas.scale(3f, 3f);
+                movie.draw(canvas, -100, 0);
+                canvas.restore();
+                holder.unlockCanvasAndPost(canvas);
+                movie.setTime((int) (System.currentTimeMillis() % movie.duration()));
+
+                handler.removeCallbacks(drawGIF);
+                handler.postDelayed(drawGIF, frameDuration);
+            }
+        }
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            this.visible = visible;
+            if (visible) {
+                handler.post(drawGIF);
+            } else {
+                handler.removeCallbacks(drawGIF);
+            }
+        }
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            handler.removeCallbacks(drawGIF);
+        }
+    }
+
 }
